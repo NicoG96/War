45e4d9fc562a5ab0af23a9f19d4b547aca53ed6d
Nico Gonzalez <NicoG96@gmail.com>
2020-08-21 11:33:49 -0400

    Remove singleton for unit tests

--- war/CardFactory.py
+++ war/CardFactory.py
@@ -2,18 +2,6 @@ from .Card import Card
 
 
 class CardFactory():
-	_instance = None
-
-	@staticmethod
-	def get_instance():
-		return CardFactory._instance
-	
-	def __init__(self):
-		if CardFactory.get_instance() == None:
-			CardFactory._instance = self
-		else:
-			raise Exception("Only one factory permitted")
-
 	def create_spade(self, value):
 		return Card(value, 'spade')
 
