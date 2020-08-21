d19d08ab876a8c8ed2149183373f32301d6a5ab0
Nico Gonzalez <NicoG96@gmail.com>
2020-08-21 16:14:24 -0400

    Add card enum

--- war/Card.py
+++ war/Card.py
@@ -1,13 +1,55 @@
+import enum
+
+class CardEnum(enum.Enum):
+	Two = 2
+	Three = 3
+	Four = 4
+	Five = 5
+	Six = 6
+	Seven = 7
+	Eight = 8
+	Nine = 9
+	Ten = 10
+	Jack = 11
+	Queen = 12
+	King = 13
+	Ace = 14
+
 class Card():
 	def __init__(self, value, suit):
-		self._value = value
+		self._enum = CardEnum(value)
 		self._suit = suit
 
 	def get_value(self):
-		return self._value
+		return self._enum.value
+
+	def get_full_name(self):
+		return self._enum.name
+
+	def get_short_name(self):
+		if self._enum.value == 11:
+			return 'J'
+		elif self._enum.value == 12:
+			return 'Q'
+		elif self._enum.value == 13:
+			return 'K'
+		elif self._enum.value == 14:
+			return 'A'
+		else:
+			return str(self._enum.value)
 	
 	def get_suit(self):
 		return self._suit
+
+	def get_symbol(self):
+		if self._suit == "spade":
+			return "♠"
+		elif self._suit == "club":
+			return "♣" 
+		elif self._suit == "heart":
+			return "♥" 
+		else:
+			return "♦"
 
 	def get_color(self):
 		return 'black' if self._suit == 'spade' or self._suit == 'club' else 'red'
