5b0203622825297126587fb59544e32121d9e9ff
Nico Gonzalez <NicoG96@gmail.com>
2020-08-21 11:35:33 -0400

    Add unit tests for war class

---
+++ tests/test_War.py
@@ -0,0 +1,31 @@
+import unittest
+from War.War import War
+import random
+
+
+class test_War(unittest.TestCase):
+	@classmethod
+	def setUpClass(cls):
+		cls.game = War()
+
+	def test_create_cards(self):
+		card_set = set()
+		cards = self.game.create_cards()
+		# make sure each card is unique
+		for card in cards:
+			if card not in card_set:
+				card_set.add(card)
+		self.assertEqual(len(cards), 52)
+		self.assertTrue(len(card_set) == 52)
+
+
+	def test_split_cards(self):
+		cards = self.game.create_cards()
+		deck1, deck2 = self.game.split_cards(cards)
+		self.assertEqual(len(deck1.get_cards()), len(deck2.get_cards()))
+		
+		for card in cards[:26]:
+			self.assertTrue(card in deck1.get_cards())
+
+		for card in cards[26:]:
+			self.assertTrue(card in deck2.get_cards())
