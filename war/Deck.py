d86b1eaaf320c5532499eddd32dff203a81ddef1
Nico Gonzalez <NicoG96@gmail.com>
2020-08-21 13:12:55 -0400

    add methods to clear deck and add multiple cards

--- war/Deck.py
+++ war/Deck.py
@@ -5,6 +5,9 @@ class Deck():
 	def add_card(self, card):
 		self._cards.append(card)
 
+	def add_cards(self, cards):
+		self._cards.extend(cards)
+
 	def remove_card(self):
 		return self._cards.pop(0)
 
@@ -13,3 +16,6 @@ class Deck():
 
 	def get_cards(self):
 		return self._cards
+
+	def clear_deck(self):
+		self._cards = []
