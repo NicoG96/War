d3220f0abb3d3387fc4decf9a4b9e9a360f61ee4
Nico Gonzalez <nicog96@gmail.com>
2020-08-22 15:43:04 -0400

    add debug flag

--- run.py
+++ run.py
@@ -2,12 +2,13 @@ from War.War import War
 import argparse
 
 
-def main(is_interactive):
-    War().start_game(is_interactive)
+def main(is_interactive, is_debug):
+    War().start_game(is_interactive, is_debug)
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--interactive", "-i", action="store_true")
+    parser.add_argument("--debug", "-d", action="store_true")
     args = parser.parse_args()
-    main(args.interactive)
+    main(args.interactive, args.debug)
